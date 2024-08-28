277ab430f46d8201f6b7497c2ac023e5bd2ca11a
shipon1242 <hossainmohammadshipon@gamil.com>
2024-08-28 23:59:00 +0600

    get data with service id

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors');
-require('dotenv').config() 
 const app = express();
+require('dotenv').config()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
 const PORT = process.env.PORT || 5000;
+// middleware
 app.use(cors())
 app.use(express.json())
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.unjawmb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
+// console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,6 +27,27 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
+    const serviceCollection = client.db('RepairDb').collection('services')
+
+    app.get('/services', async (req, res) => {
+      const result = await serviceCollection.find().toArray()
+      res.send(result)
+    })
+    app.post('/services',async(req,res)=>{
+      const services =req.body
+      // console.log(services)
+      const result =await serviceCollection.insertOne(services)
+      res.send(result)
+    })
+app.get("/services/:id",async(req,res)=>{
+   const id = req.params.id
+   const query={_id:new ObjectId(id)}
+   const result = await serviceCollection.findOne(query)
+   res.send(result)
+   
+})
+    
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -37,8 +59,8 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Electronic item is running')
+  res.send('Electronic item is running')
 })
 app.listen(PORT, () => {
-    console.log(`server is running on port${PORT}`)
+  console.log(`server is running on port${PORT}`)
 })
