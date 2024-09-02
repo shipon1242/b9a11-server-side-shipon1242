9a8207e4cd059e4dac767dd183a3306d687dea00
shipon1242 <hossainmohammadshipon@gamil.com>
2024-09-02 10:02:03 +0600

    delete operation done

--- index.js
+++ index.js
@@ -93,6 +93,13 @@ app.put("/services/:id",async(req,res)=>{
   res.send(result)
 })
 
+app.delete('/services/:id',async(req,res)=>{
+ const id =req.params.id;
+ const query ={_id:new ObjectId(id)}
+ const result =await serviceCollection.deleteOne(query) 
+  res.send(result)
+})
+
     
     
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
